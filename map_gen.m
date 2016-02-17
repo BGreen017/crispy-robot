fe11d85563d215df369861d87bd621a085ead18a
Karl Hill <khill35@umd.edu>
2016-02-17 18:19:17 -0500

    Update map_gen.m

--- map_gen.m
+++ map_gen.m
@@ -7,13 +7,15 @@ Terrain = zeros(N);
 % Waypoints = ceil((N)*(rand(2,10)));
 Waypoints = [2 3 8 5 1 9 ; 7 4 1 8 8 3];
 for ii = 1:length(Waypoints)
-    Terrain(Waypoints(1,ii),Waypoints(2,ii)) = inf;
+    Terrain(Waypoints(1,ii),Waypoints(2,ii)) = 100;
 end
 
 Goal = [2 9];
 Start = [2 2];
 
-Terrain(Goal(1),Goal(2)) = 1;
-Terrain(Start(1),Start(2)) = 1;
+Terrain(Goal(1),Goal(2)) = 30;
+Terrain(Start(1),Start(2)) = 60;
 
-Terrain
+figure 
+surf(Terrain)
+grid on
